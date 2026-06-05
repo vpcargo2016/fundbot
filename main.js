975e11da0cb0eee7d24090648f732f4abe21592e
vpcargo2016 <vpcargo2016@gmail.com>
2026-06-05 10:31:58 +0300

    Исправил инициализацию AppKit

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 import { createAppKit } from '@reown/appkit';
+import { EthersAdapter } from '@reown/appkit-adapter-ethers';
+import { mainnet, arbitrum } from '@reown/appkit/networks'; // Импортируем сети
 
 const modal = createAppKit({
+  adapters: [new EthersAdapter()],
+  networks: [mainnet, arbitrum],
   projectId: 'b302fccaf80093ef6cf8cad5fd4fc3ff',
   metadata: {
     name: 'AML Bot',
     description: 'Check',
     url: 'https://freebotaml.org',
     icons: ['https://avatars.githubusercontent.com/u/37784886']
-  },
-  networks: []
+  }
 });
 
 document.getElementById('connect-btn').addEventListener('click', () => {
